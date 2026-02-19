7b2e5a838f6de2aa2e316db209fd7aee3533da88
Mehakjot Singh <mehakjot1971@gmail.com>
2026-02-19 22:25:00 +0530

    style: Remove unnecessary blank lines for improved readability.

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ function rollDice() {
 
     for (let i = 0; i < diceCount; i++) {
         const roll = Math.floor(Math.random() * 6) + 1;
-
         counts.push(roll);
         images.push(`<img src="dice_images/dice-${roll}.png" alt="Dice" width="100" height="100">`);
-       
     }
-
     count.innerHTML = `Count in dices: ${counts.join(', ')}`;
     diceImagesDiv.innerHTML = images.join(' ');
 
